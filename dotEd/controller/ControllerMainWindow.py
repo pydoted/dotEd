8e0146c86c1cc5d8630121ea6e6bcaef0deccc01
M.Lass <morvan.lassauzay@orange.fr>
2016-01-30 22:36:40 +0100

    ajout ControllerMainWindows (pas passé au commit précédent par oublie)

---
+++ dotEd/controller/ControllerMainWindow.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from controller.Controller import Controller
+
+class ControllerMainWindow(Controller):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, model, view):
+        '''
+        Constructor
+        '''
+        Controller.__init__(self, model, view)
+        
+    def actualise(self):
+        # a completer
+        pass
